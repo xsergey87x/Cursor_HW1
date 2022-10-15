5c48cbaa19dbabdeb2988cd8fe958187d7b8ed0d
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-10-15 18:09:49 +0300

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import arifmetic.SimpleMath;
 import wildAnimal.WildAnimal;
 
 public class Main {
@@ -8,7 +9,15 @@ public class Main {
     animal.catSay();
     animal.dogSay();
 
+    Math math = new Math();
+        System.out.println(math.add(5,2));
+        System.out.println(math.subtraction(10,5));
+
     WildAnimal wildAnimal = new WildAnimal();
     wildAnimal.tigerSay();
+
+    SimpleMath simpleMath = new SimpleMath();
+        System.out.println(simpleMath.divide(15,3));
+        System.out.println(simpleMath.multiplication(3,4));
     }
 }
