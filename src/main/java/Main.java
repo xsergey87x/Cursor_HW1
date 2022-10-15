9fbf5451e3bb37f07c22b60de9b3b6dc40710a1d
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-10-15 17:37:30 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import wildAnimal.WildAnimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Animal animal = new Animal();
+    animal.catSay();
+    animal.dogSay();
+
+    WildAnimal wildAnimal = new WildAnimal();
+    wildAnimal.tigerSay();
+    }
+}
